853069b37a84f98277dd86d1c2176d1a57ed7850
derhannesb <johannes.bade@gmail.com>
2016-10-01 02:21:05 +0200

    change movement parameters

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,7 +4,7 @@ var player_number = 0
 var joy_tresh = 0.2
 var jump_ready = true
 var jump_timer = 0.0
-var jump_time = 2.0
+var jump_time = 1.0
 
 func _ready():
 	get_node("PlayerBody/RayCast2D").add_exception(get_node("PlayerBody"))
@@ -22,12 +22,12 @@ func _fixed_process(delta):
 
 func handle_input(delta):
 	if Input.get_joy_axis(player_number,  0) < -joy_tresh:
-		get_node("PlayerBody").apply_impulse(Vector2(0,30), Vector2(-10, 0))
+		get_node("PlayerBody").apply_impulse(Vector2(0,30), Vector2(-20, 0))
 	elif Input.get_joy_axis(player_number,  0) > joy_tresh:
-		get_node("PlayerBody").apply_impulse(Vector2(0,30), Vector2(10, 0))
+		get_node("PlayerBody").apply_impulse(Vector2(0,30), Vector2(20, 0))
 	else:
 		get_node("PlayerBody").apply_impulse(Vector2(0,0), Vector2(-get_node("PlayerBody").get_linear_velocity().x*0.5, 0))
 	if jump_ready && Input.get_joy_axis(player_number, 1) < -joy_tresh:
-		get_node("PlayerBody").apply_impulse(Vector2(0, 30), Vector2(0, -200))
+		get_node("PlayerBody").apply_impulse(Vector2(0, 30), Vector2(0, -1100))
 		jump_ready = false
 		print("Jump")
