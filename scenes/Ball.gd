4bd0dc669ba6d5ffcad2b78eb328e275783e2676
derhannesb <hannes@dasletzteland.de>
2016-10-08 13:37:44 +0200

    add ball gravity when hit

---
+++ scenes/Ball.gd
@@ -0,0 +1,13 @@
+
+extends Node2D
+var body
+
+func _ready():
+	body = get_node("RigidBody2D")
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	if (body.get_gravity_scale() == 0):
+		if (body.get_colliding_bodies().size() > 0):
+			body.set_gravity_scale(1)
+	
